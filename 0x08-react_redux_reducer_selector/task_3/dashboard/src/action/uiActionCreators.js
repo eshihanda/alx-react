bad2206a4f9645f4250f697d9fd625d27421d007
eshihanda <eshihandamonn@gmail.com>
2024-09-25 23:16:50 +0300

    notifications reducer

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/action/uiActionCreators.js
@@ -0,0 +1,71 @@
+import {
+	LOGIN,
+	LOGOUT,
+	DISPLAY_NOTIFICATION_DRAWER,
+	HIDE_NOTIFICATION_DRAWER,
+	LOGIN_FAILURE,
+	LOGIN_SUCCESS,
+  } from './uiActionTypes';
+
+  export const login = (email, password) => {
+	return {
+		type: LOGIN,
+		user: {email, password},
+	};
+  };
+  export const logout = () => {
+	return {
+	  type: LOGOUT,
+	};
+  };
+
+  export const displayNotificationDrawer = () => {
+	return {
+	  type: DISPLAY_NOTIFICATION_DRAWER,
+	};
+  };
+
+  export const hideNotificationDrawer = () => {
+	return {
+	  type: HIDE_NOTIFICATION_DRAWER,
+	};
+  };
+
+
+  export const loginFailure = () => {
+	return {
+		type: LOGIN_FAILURE,
+	};
+  };
+  export const loginSuccess = () => {
+	return {
+		type: LOGIN_SUCCESS,
+	};
+  };
+
+  export const loginRequest = (email, password) => {
+	return (dispatch) => {
+		fetch('/public/login_success.json')
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		});
+		
+		.then((data) => {
+			// Assuming your API response has a structure that indicates success or failure
+			if (data.email === email && data.password === password) {
+			  dispatch(loginSuccess());
+			} else {
+			  dispatch(loginFailure());
+			}
+		  })
+		  .catch((error) => {
+			console.error('There was an error with the login request:', error);
+			dispatch(loginFailure());
+		  });
+	};
+  };
+
+
